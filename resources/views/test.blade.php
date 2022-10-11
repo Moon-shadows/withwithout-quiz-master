5895eb674f2dda49182f044148c0fdd54715bdee
Maria Lohman <marialohman1@hotmail.com>
2022-10-11 18:39:14 +0200

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,54 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Donkeyquiz - boilerplate</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet"> 
+    </head>
+    <body >
+
+
+
+<div class="grid-3-columns grid grid-cols-3">
+          <div class="column-1">
+                <div>
+                    <img 
+                        src="/Image/Logo(3).svg"
+                        class="hover:bg-mörkblå h-37.5 pt-6 pb-56 pl-14">  <!-- Hover over Logo? -->
+                </div>
+                <div>
+                    <img 
+                        src="/Image/blob-left.png"
+                        class="hover:bg-mörkblå h-96">  <!-- Hover over Logo? -->
+                </div>
+          </div> 
+     
+
+          <div class="column-2 pt-80">
+                <h1 class="text-mörkblå font-poppins text-48 font-semibold text-center mb-8"> 
+                    Svensk mästare i TP?
+                </h1>
+                <p class="text-20 text-center mb-10"> Utmana vänner, kollegor och familj på frågesport. 
+                    Svara på 35 samtida frågor i 7 olika kategorier. </p>
+                <div class="button-container w-63 text-center">
+                    <button class="py-8 px-3 rounded-full bg-white text-ljusblå border-2 border-ljusblå" onclick="myFunction()">Klicka här för att starta</button>
+                </div>
+          </div>
+
+          <div class="column-3">
+                <img 
+                  src="/Image/blob-right.png"
+                  class="hover:bg-mörkblå h-96 pt-20">  <!-- Hover over Logo? -->
+          </div>
+
+</div>
+
+            <!-- Detta ska va kvar-->
+        <script type="module" crossorigin src="http://localhost:3000/@@vite/client"></script>
+        <script type="module" crossorigin src="http://localhost:3000/resources/js/app.js"></script>
+        </section>
+    
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  corePlugins: {
+    container: false
+  },
+
+  content: [
+    './resources/js/**/*.{vue, js, ts, jsx, tsx}',
+    './resources/views/**/*.php'
+  ],
+  safelist: [],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1440px'
+    },
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#fff',
+      black: '#000',
+      red: '#881337',
+      mörkblå: '#000064',
+      ljusblå: '#7678ED',
+      
+
+  
+    },
+
+    fontFamily: {
+      'poppins': ["Poppins", 'sans-serif'],
+    },
+
+    fontSize: {
+      16: '1rem',
+      20: '1.25rem',
+      48: '3rem'
+    },
+
+    extend: {
+      // ..
+    }
+  },
+  plugins: [
+    // ..
+  ]
+}
