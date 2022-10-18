604cd5c62a2c16aae041b72ff044046168d1b577
Maria Lohman <marialohman1@hotmail.com>
2022-10-18 15:07:24 +0200

    add route questions

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,70 @@ Route::get('/', function () {
 });
 
 Route::get('/questions', function () {
-    return view('welcome');
+    $questions = [];
+    $getJson = file_get_contents(storage_path("questions.json"));
+    $jsonQuestions = json_decode($getJson, true);
+    $countCategories = ["Film & TV"=>[], "Geografi"=>[], "Historia"=>[], "Musik"=>[], "Övrigt"=>[], "Vetenskap"=>[], "Sport"=>[]];
+  
+    
+
+while (count($questions) < 35){
+
+    $randomNumber = rand(0, count($jsonQuestions) -1);
+    $randomQuestion = $jsonQuestions[$randomNumber];
+    $category = $jsonQuestions[$randomNumber]["category"];
+
+
+    //kolla efter dubbletter
+
+    if(in_array($randomQuestion, $questions)){
+        continue;
+}
+if($category ==='Film & TV' && count($countCategories['Film & TV']) < 5){
+        array_push($countCategories["Film & TV"],1);
+}
+elseif($category ==='Film & TV' && count($countCategories['Film & TV']) === 5){
+   continue;
+}
+if($category ==='Geografi' && count($countCategories['Geografi']) < 5){
+    array_push($countCategories["Geografi"],1);
+}
+elseif($category ==='Geografi' && count($countCategories['Geografi']) === 5){
+continue;
+}
+if($category ==='Historia' && count($countCategories['Historia']) < 5){
+    array_push($countCategories["Historia"],1);
+}
+elseif($category ==='Historia' && count($countCategories['Historia']) === 5){
+continue;
+}
+if($category ==='Musik' && count($countCategories['Musik']) < 5){
+    array_push($countCategories["Musik"],1);
+}
+elseif($category ==='Musik' && count($countCategories['Musik']) === 5){
+continue;
+}
+if($category ==='Övrigt' && count($countCategories['Övrigt']) < 5){
+    array_push($countCategories["Övrigt"],1);
+}
+elseif($category ==='Övrigt' && count($countCategories['Övrigt']) === 5){
+continue;
+}
+if($category ==='Vetenskap' && count($countCategories['Vetenskap']) < 5){
+    array_push($countCategories["Vetenskap"],1);
+}
+elseif($category ==='Vetenskap' && count($countCategories['Vetenskap']) === 5){
+continue;
+}
+if($category ==='Sport' && count($countCategories['Sport']) < 5){
+    array_push($countCategories["Sport"],1);
+}
+elseif($category ==='Sport' && count($countCategories['Sport']) === 5){
+continue;
+}
+
+
 });
+
+
+
